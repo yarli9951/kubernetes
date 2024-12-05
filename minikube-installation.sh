c5010b9d6f24461dfb83f804976b6715cc97f072
vamsi <scvamsi@gmail.com>
2024-12-04 20:06:32 -0500

    adding files

---
+++ minikube-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum install -y curl conntrack-tools
+sudo yum install -y VirtualBox
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+curl -LO "https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+minikube start --driver=virtualbox
+minikube start --driver=docker
+minikube status
+kubectl get nodes
+minikube dashboard
